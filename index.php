be303f0a201f21e7976860a1c74a53e50eb4cfec
Héloïse Chauvel <chauvel.h@gmail.com>
2017-08-03 16:30:13 +0200

    Index css - completely changed design

--- index.php
+++ index.php
@@ -50,131 +50,109 @@
           </div>
       </nav>
 
-      <div id="carousel-sports" class="carousel slide" data-ride="carousel">
-          <div class="carousel-inner" role="listbox" >
-              <div class="carousel-item active">
-                  <div class="slider-size">
-                    <img class="d-block img-fluid" src="assets/img/motocross.jpg" alt="Second slide">
+      <div class="container-fluid">
+          <div class="row">
+              <div class="col-lg-7 carousel">
+                  <div class="col-lg-12">
+                      <h2>Défiez vos limites avec OsePose !</h2>
+                  </div>
+                  <div id="carousel-sports" class="carousel slide" data-ride="carousel">
+                      <div class="carousel-inner" role="listbox" >
+                          <div class="carousel-item active">
+                              <div class="slider-size">
+                                <img class="d-block img-fluid" src="assets/img/motocross.jpg" alt="Second slide">
+                              </div>
+                          </div>
+                          <div class="carousel-item">
+                              <div class="slider-size">
+                                  <img class="d-block img-fluid" src="assets/img/parkour.JPG" alt="Third slide">
+                              </div>
+                          </div>
+                          <div class="carousel-item">
+                              <div class="slider-size">
+                                <img class="d-block img-fluid" src="assets/img/soccer.jpg" alt="Third slide">
+                              </div>
+                          </div>
+                          <div class="carousel-item">
+                              <div class="slider-size">
+                                <img class="d-block img-fluid" src="assets/img/surf.jpg" alt="Third slide">
+                              </div>
+                          </div>
+                      </div>
                   </div>
               </div>
-              <div class="carousel-item">
-                  <div class="slider-size">
-                      <img class="d-block img-fluid" src="assets/img/parkour.JPG" alt="Third slide">
-                  </div>
-              </div>
-              <div class="carousel-item">
-                  <div class="slider-size">
-                    <img class="d-block img-fluid" src="assets/img/soccer.jpg" alt="Third slide">
-                  </div>
-              </div>
-              <div class="carousel-item">
-                  <div class="slider-size">
-                    <img class="d-block img-fluid" src="assets/img/surf.jpg" alt="Third slide">
-                  </div>
-              </div>
-          </div>
-      </div>
 
-          <div class="panel-content">
-              <form role="form" method="post" action="inscription.php" id="inscription-ok">
-                  <input type="hidden" name="inscription" value="1" />
-
-                  <div class="form-group">
-                      <div class="label-perso">
-                          <label for="email-i"> Adresse mail : </label>
-                      </div>
-                      <div class="input-perso">
-                          <input type="email" name="email" class="form-control" id="email-i">
+              <div class="col-lg-5 inscription">
+                  <form role="form" method="post" action="inscription.php" id="inscription-ok" class="form-horizontal">
+                      <div class="col-lg-12">
+                        <h2>Inscription</h2>
                       </div>
 
-                      <!-- ENCART ERREUR MAIL -->
-                      <div id="noemail" class="erreur"> Merci de bien vouloir renseigner une adresse e-mail. </div>
-                      <div id="falseemail" class="erreur"> Merci de bien vouloir renseigner une adresse e-mail valide. </div>
-                      <div id="exist-email" class="erreur"> L'adresse e-mail a déjà été utilisée pour une inscription, merci d'utiliser l'outil mot de passe oublié. </div>
-                      <!-- ENCART ERREUR MAIL -->
+                      <input type="hidden" name="inscription" value="1" />
 
-                  </div>
-
-
-                  <div class="form-group">
-                      <div class="label-perso">
-                          <label for="login-i">Login : </label>
-                      </div>
-                      <div class="input-perso">
-                          <input type="text" name="login" class="form-control" id="login-i">
+                      <div class="form-group">
+                          <div class="col-lg-12">
+                              <label for="email-i"> Adresse mail : </label>
+                              <input type="email" name="email" class="form-control" id="email-i">
+                          </div>
+                          <!-- ENCART ERREUR MAIL -->
+                          <div id="noemail" class="erreur"> Merci de bien vouloir renseigner une adresse e-mail. </div>
+                          <div id="falseemail" class="erreur"> Merci de bien vouloir renseigner une adresse e-mail valide. </div>
+                          <div id="exist-email" class="erreur"> L'adresse e-mail a déjà été utilisée pour une inscription, merci d'utiliser l'outil mot de passe oublié. </div>
+                          <!-- ENCART ERREUR MAIL -->
                       </div>
 
+                      <div class="form-group">
+                          <div class="col-lg-12">
+                              <label for="login-i">Login : </label>
+                              <input type="text" name="login" class="form-control" id="login-i">
+                          </div>
 
-                      <!-- ENCART ERREUR LOGIN -->
-                      <div id="nologin" class="erreur"> Merci de bien vouloir renseigner un login. </div>
-                      <div id="length-login" class="erreur"> Le login doit être composé d'au moins 6 caractères. </div>
-                      <div id="exist-login" class="erreur"> Le login a déjà été utilisé pour une inscription, merci d'utiliser l'outil mot de passe oublié. </div>
-                      <!-- ENCART ERREUR LOGIN -->
-
-
-                  </div>
-
-
-                  <div class="form-group">
-                      <div class="label-perso">
-                          <label for="pwd">Mot de passe : </label>
-                      </div>
-                      <div class="input-perso">
-                          <input type="password" name="password" class="form-control" id="pwd-i">
+                          <!-- ENCART ERREUR LOGIN -->
+                          <div id="nologin" class="erreur"> Merci de bien vouloir renseigner un login. </div>
+                          <div id="length-login" class="erreur"> Le login doit être composé d'au moins 6 caractères. </div>
+                          <div id="exist-login" class="erreur"> Le login a déjà été utilisé pour une inscription, merci d'utiliser l'outil mot de passe oublié. </div>
+                          <!-- ENCART ERREUR LOGIN -->
                       </div>
 
+                      <div class="form-group">
+                          <div class="col-lg-12">
+                              <label for="pwd-i">Mot de passe : </label>
+                              <input type="password" name="password" class="form-control" id="pwd-i">
+                          </div>
 
-                      <!-- ENCART ERREUR PWD1 -->
-                      <div id="nopwd1" class="erreur"> Merci de bien vouloir renseigner un mot de passe. </div>
-                      <div id="length-pwd1" class="erreur"> Le mot de passe est trop court pour des raisons de sécurité merci de renseigner un mot de passe d'au minimum 8 caractères. </div>
-                      <!-- ENCART ERREUR PWD1 -->
-
-
-                  </div>
-
-
-                  <div class="form-group">
-                      <div class="label-perso">
-                          <label for="pwd2">Confirmer votre mot de passe : </label>
-                      </div>
-                      <div class="input-perso">
-                          <input type="password" class="form-control" id="pwd2-i">
+                          <!-- ENCART ERREUR PWD1 -->
+                          <div id="nopwd1" class="erreur"> Merci de bien vouloir renseigner un mot de passe. </div>
+                          <div id="length-pwd1" class="erreur"> Le mot de passe est trop court pour des raisons de sécurité merci de renseigner un mot de passe d'au minimum 8 caractères. </div>
+                          <!-- ENCART ERREUR PWD1 -->
                       </div>
 
-                      <!-- ENCART ERREUR PWD1 -->
-                      <div id="nopwd2" class="erreur"> Il vous faut confirmer votre mot de passe. </div>
-                      <div id="same-pwd2" class="erreur"> Le mot de passe renseigné n'est pas identique au précédent. </div>
-                      <!-- ENCART ERREUR PWD1 -->
+                      <div class="form-group">
+                          <div class="col-lg-12">
+                              <label for="pwd2-i">Confirmer votre mot de passe : </label>
+                              <input type="password" class="form-control" id="pwd2-i">
+                          </div>
 
-
-                  </div>
-
-                  <div class="form-group">
-                      <div class="label-perso">
-                          <label>Vous êtes  </label>
-                      </div>
-                      <div class="input-perso">
-                          <input type="radio" name="gender" value="F" id="F" /> Femme
-                          <input type="radio" name="gender" value="H" id="H" /> Homme
+                          <!-- ENCART ERREUR PWD1 -->
+                          <div id="nopwd2" class="erreur"> Il vous faut confirmer votre mot de passe. </div>
+                          <div id="same-pwd2" class="erreur"> Le mot de passe renseigné n'est pas identique au précédent. </div>
+                          <!-- ENCART ERREUR PWD1 -->
                       </div>
 
+                      <div class="form-group">
+                          <div class="col-lg-12">
+                              <label>Vous êtes  </label>
+                              <input type="radio" name="gender" value="F" id="F" /> Femme
+                              <input type="radio" name="gender" value="H" id="H" /> Homme
+                          </div>
+                          <!-- ENCART ERREUR SEX -->
+                          <div id="nosex" class="erreur"> Merci de bien vouloir renseigner votre sexe. </div>
+                          <!-- ENCART ERREUR SEX -->
+                      </div>
 
-                      <!-- ENCART ERREUR SEX -->
-                      <div id="nosex" class="erreur"> Merci de bien vouloir renseigner votre sexe. </div>
-                      <!-- ENCART ERREUR SEX -->
-
-
-                  </div>
-
-
-                  <div class="form-group">
-                      <div class="label-perso">
+                      <div class="form-group">
                           <label>Date de naissance  </label>
-                      </div>
-                      <div class="input-perso">
-                          <select id="day" name="day">
-
-                          </select>
+                          <select id="day" name="day"></select>
 
                           <select id="month" name="month">
                               <option value="01"> Janvier </option>
@@ -200,16 +178,9 @@
                       <div id="noyear" class="erreur"> Merci de renseigner votre année de naissance. </div>
                       <!-- ENCART ERREUR SEX -->
 
-
-
-                  </div>
-
-                  <br />
-                  <div class="form-group">
-                      <div class="label-perso">
-                          <label for="pwd2">Les sports que vous aimez : </label>
-                      </div>
-                      <div class="input-perso">
+                      <br />
+                      <div class="form-group">
+                          <label>Les sports que vous aimez : </label>
                           <div id="multiple-select" >
                               Listes déroulantes des sports <img src="assets/img/arrow.png" class="img-responsive" style="float:right;margin-top:2%;" id="arrow" />
                           </div>
@@ -229,17 +200,11 @@
                           </div>
                       </div>
 
-                  </div>
-
-                  <center>
-                      <div class="input-button-perso">
-                          <input type="button" class="btn-perso" id="inscription" value="S'inscrire" />
-                      </div>
-                  </center>
-
-
-              </form>
+                      <button class="btn" id="inscription"> S'inscrire </button>
+                  </form>
+              </div>
           </div>
+      </div>
 
       <!-- jQuery CDN -->
       <script src="http://code.jquery.com/jquery-3.2.1.min.js"></script>
